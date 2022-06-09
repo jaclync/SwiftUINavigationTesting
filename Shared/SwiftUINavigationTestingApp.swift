f7c8e66164242ec38bad17450bd58c5e45e38391
Jaclyn Chen <watertranquil@gmail.com>
2022-06-09 22:03:51 +0800

    Initial Commit

---
+++ Shared/SwiftUINavigationTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationTestingApp.swift
+//  Shared
+//
+//  Created by Jaclyn Chen on 2022/6/9.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
